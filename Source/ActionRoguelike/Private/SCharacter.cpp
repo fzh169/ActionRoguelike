292f83fe851564124b92601b2a759ad2b4839996
范泽华 <fzh169@126.com>
2023-02-01 19:08:58 +0800

    第二章 版本和项目 03

--- Source/ActionRoguelike/Private/SCharacter.cpp
+++ Source/ActionRoguelike/Private/SCharacter.cpp
@@ -26,6 +26,11 @@ void ASCharacter::BeginPlay()
 	
 }
 
+void ASCharacter::MoveForward(float value)
+{
+	AddMovementInput(GetActorForwardVector(), value);
+}
+
 // Called every frame
 void ASCharacter::Tick(float DeltaTime)
 {
@@ -38,5 +43,8 @@ void ASCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponen
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	PlayerInputComponent->BindAxis("MoveForward", this, &ASCharacter::MoveForward);
+
+	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
 }
 
